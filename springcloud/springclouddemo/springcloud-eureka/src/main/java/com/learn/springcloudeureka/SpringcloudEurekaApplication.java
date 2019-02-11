45566b4c6295b8a807e50fb371e5a413faeaacd5
yinjinquan <yinjinquanu@163.com>
2019-02-11 17:19:23 +0800

    增加学习代码

---
+++ springcloud/springclouddemo/springcloud-eureka/src/main/java/com/learn/springcloudeureka/SpringcloudEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.learn.springcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaApplication.class, args);
+    }
+
+}
+
